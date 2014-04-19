2c93fc95c34bc6b0e7482de97f77379f24da6740
Pierre Le Gall <pierre@legall.im>
2014-04-19 19:52:53 +0200

    Visitor pattern sources group together in visitor.rb (module VisitorPattern)

--- visitor.rb
+++ visitor.rb
@@ -1,19 +1,39 @@
-require './visitable'
-require './no_visit_method_error'
+# require './visitable'
+# require './no_visit_method_error'
 
-module Visitor
-  def visit object, klass=object.class
-    visit_method = visit_method_for klass
-    if self.respond_to? visit_method
-      send visit_method, object
-    else
-      raise NoVisitMethodError.new self, object if klass.nil?
-      visit object, klass.superclass
+module VisitorPattern
+  module Visitor
+    def visit object, klass=object.class
+      visit_method = visit_method_for klass
+      if self.respond_to? visit_method
+        send visit_method, object
+      else
+        raise NoVisitMethodError.new self, object if klass.nil?
+        visit object, klass.superclass
+      end
     end
   end
   
-  private
-
+  module Visitable
+    def accept visitor
+      visitor.visit self
+    end
+  end
+  
+  class NoVisitMethodError < NoMethodError
+    attr_reader :visitor, :visitable
+    
+    def initialize visitor, visitable
+      @visitor   = visitor
+      @visitable = visitable
+    end
+    
+    def message
+      "There is no method to visit #{@visitable.class} " \
+      "objects in the #{@visitor.class} class"
+    end
+  end
+  
   def visit_method_for klass
     method = "visit_#{klass}".gsub(/::/, '_')
     return method.intern
--- visitor_spec.rb
+++ visitor_spec.rb
@@ -1,12 +1,39 @@
-require './visitable'
 require './visitor'
 
-describe Visitor do
+include VisitorPattern
+
+describe VisitorPattern do
   before :all do
     Object.const_set :VisitorTest, Class.new
     VisitorTest.include Visitor
     String.include Visitable
     @visitor = VisitorTest.new
+  end
+  
+  describe Visitor do
+    describe '#visit' do
+      context 'when visit method exists' do
+        before :all do
+          @visitor.class.send :define_method, :visit_String do |string|
+            return String
+          end
+        end
+        it { (@visitor.respond_to? :visit_String).should be_true }
+        it { @visitor.visit(String.new).should == String }
+      end
+      
+      context 'when visit method does not exist' do
+        it 'raise NoVisitMethodError' do
+          expect { @visitor.visit(Array.new) }.to raise_error NoVisitMethodError
+        end
+      end
+    end
+  end
+  
+  describe Visitable do
+    describe '#accept' do
+      # TO DO... no idea :d
+    end
   end
   
   describe '#visit_method_for' do
@@ -16,31 +43,13 @@ describe Visitor do
       A::B.const_set   :C, Class.new
     end
     
-    it { @visitor.send(:visit_method_for, String).should == :'visit_String' }
-    it { @visitor.send(:visit_method_for, A::B::C).should == :'visit_A_B_C' }
-    it { @visitor.send(:visit_method_for, String).should_not == :'visit_string' }
-    it { @visitor.send(:visit_method_for, A::B::C).should_not == :'visit_C' }
-    it { @visitor.send(:visit_method_for, A::B::C).should_not == :'visit_A' }
-    it { @visitor.send(:visit_method_for, A::B::C).should_not == :'visit_A_B' }
-    it { @visitor.send(:visit_method_for, A::B::C).should_not == :'visit_a_b_c' }
-  end
-  
-  describe '#visit' do
-    context 'when visit method exists' do
-      before :all do
-        @visitor.class.send :define_method, :visit_String do |string|
-          return String
-        end
-      end
-      it { (@visitor.respond_to? :visit_String).should be_true }
-      it { @visitor.visit(String.new).should == String }
-    end
-
-    context 'when visit method does not exist' do
-      it 'raise NoVisitMethodError' do
-        expect { @visitor.visit(Array.new) }.to raise_error NoVisitMethodError
-      end
-    end
+    it { visit_method_for(String).should == :'visit_String' }
+    it { visit_method_for(A::B::C).should == :'visit_A_B_C' }
+    it { visit_method_for(String).should_not == :'visit_string' }
+    it { visit_method_for(A::B::C).should_not == :'visit_C' }
+    it { visit_method_for(A::B::C).should_not == :'visit_A' }
+    it { visit_method_for(A::B::C).should_not == :'visit_A_B' }
+    it { visit_method_for(A::B::C).should_not == :'visit_a_b_c' }
   end
 end
 
