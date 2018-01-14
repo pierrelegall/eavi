9779bbcd4913b7f195d7c057010b70c0ea4766b2
Pierre Le Gall <pierre@legall.im>
2018-01-14 14:20:31 +0100

    Use fixtures in NoVisitMethodError tests

--- test/no_visit_method_error_test.rb
+++ test/no_visit_method_error_test.rb
@@ -1,11 +1,12 @@
 require 'minitest/autorun'
 
 require_relative '../lib/eavi/no_visit_method_error'
+require_relative 'fixtures'
 
 describe Eavi::NoVisitMethodError do
   it 'is catched as TypeError' do
     assert_raises TypeError do
-      raise Eavi::NoVisitMethodError.new(nil, nil, nil)
+      raise Eavi::NoVisitMethodError.new(Reader, 'string', String)
     end
   end
 end
